414e262655f4d9668e5692f8713b0b98ce83b74a
jerryJK <jaroslawkuliga@gmail.com>
2018-01-16 20:49:23 +0100

    add section restaurant

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+$(document).on('ready', function() {
+    $('.center').slick({
+        centerMode: true,
+        //cssEase: 'cubic-bezier(0.950, 0.050, 0.795, 0.035)',
+        easing: 'easeOutElastic',
+        focusOnSelect: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '60px',
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }]
+    });
+});
